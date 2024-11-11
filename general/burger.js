6f08c9ac29a5fb00f8d98deed1f73bec0e4fe698
paradoch <167754119+paradoch@users.noreply.github.com>
2024-11-11 12:54:24 +0300

    feat: update file

--- general/burger.js
+++ general/burger.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
     const burgerMenu = document.getElementById('burgerMenu');
     const headerBlock = document.getElementById('headerBlock');
-  
+   
     burgerMenu.addEventListener('click', () => {
       burgerMenu.classList.toggle('open');
       headerBlock.classList.toggle('open');
